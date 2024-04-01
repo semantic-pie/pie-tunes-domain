dbf53bb5c30ad051ea596e81a7b4c6452c4a354b
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-01 18:11:12 +0300

    refactor: return mono void instead of string

--- src/main/java/com/apipietunes/clients/controllers/TrackLoaderController.java
+++ src/main/java/com/apipietunes/clients/controllers/TrackLoaderController.java
@@ -1,17 +1,17 @@
 package com.apipietunes.clients.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 
 import com.apipietunes.clients.services.TrackLoaderService;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
@@ -20,17 +20,19 @@ public class TrackLoaderController {
 
   private final TrackLoaderService trackLoaderService;
 
+  @ResponseStatus(HttpStatus.CREATED)
   @Operation(description = "Upload multiple multipart files. (overflow is possible)")
   @PostMapping(value = "/track-loader/upload", consumes = "multipart/form-data")
-  public Mono<String> handleFileUpload(@RequestPart("file") Flux<FilePart> filePartFlux) {
+  public Mono<Void> handleFilesUpload(@RequestPart("file") Flux<FilePart> filePartFlux) {
     return filePartFlux.collectList()
         .flatMap(trackLoaderService::saveAll)
-        .then(Mono.just("Uploaded"));
+        .then();
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @Operation(description = "Upload single multipart file.")
   @PostMapping(value = "/track-loader/upload-one", consumes = "multipart/form-data")
-  public Mono<String> handleFileUpload(@RequestPart("file") Mono<FilePart> filePartFlux) {
-    return filePartFlux.flatMap(trackLoaderService::save).then(Mono.just("Uploaded"));
+  public Mono<Void> handleFileUpload(@RequestPart("file") Mono<FilePart> filePartFlux) {
+    return filePartFlux.flatMap(trackLoaderService::save).then();
   }
 }
